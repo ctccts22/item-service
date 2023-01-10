22b707bc4b0c2ad9244adcd039e5a800436cdbfc
ctccts22 <ctccts22@gmail.com>
2023-01-10 23:09:25 +0900

    xx

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -5,12 +5,9 @@ import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.web.bind.annotation.*;
 import javax.annotation.PostConstruct;
 import java.util.List;
-
 @Controller
 @RequestMapping("/basic/items")
 @RequiredArgsConstructor
@@ -22,6 +19,61 @@ public class BasicItemController {
         model.addAttribute("items", items);
         return "basic/items";
     }
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+    @GetMapping("/add")
+    public String addForm() {
+        return "basic/addForm";
+    }
+//    @PostMapping("/add")
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item =new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가 생략가능
+
+        return "basic/item";
+    }
+//    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+    @PostMapping("/{itemId}/edit")
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) {
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}";
+    }
     /**
      * 테스트용 데이터 추가
      */
@@ -30,5 +82,4 @@ public class BasicItemController {
         itemRepository.save(new Item("testA", 10000, 10));
         itemRepository.save(new Item("testB", 20000, 20));
     }
-
 }
--- src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
+++ src/test/java/hello/itemservice/domain/item/ItemRepositoryTest.java
@@ -2,57 +2,48 @@ package hello.itemservice.domain.item;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
-
+import static org.assertj.core.api.Assertions.assertThat;
 class ItemRepositoryTest {
-
     ItemRepository itemRepository = new ItemRepository();
-
     @AfterEach
     void afterEach() {
         itemRepository.clearStore();
     }
-
     @Test
     void save() {
-        //given
+//given
         Item item = new Item("itemA", 10000, 10);
-        //when
+//when
         Item savedItem = itemRepository.save(item);
-        //then
-        Item findItem = itemRepository.findBy(item.getId());
+//then
+        Item findItem = itemRepository.findById(item.getId());
         assertThat(findItem).isEqualTo(savedItem);
     }
     @Test
     void findAll() {
-        //given
+//given
         Item item1 = new Item("item1", 10000, 10);
         Item item2 = new Item("item2", 20000, 20);
-
         itemRepository.save(item1);
         itemRepository.save(item2);
-        //when
+//when
         List<Item> result = itemRepository.findAll();
-        //then
+//then
         assertThat(result.size()).isEqualTo(2);
-        assertThat(result).contains(item1,item2);
+        assertThat(result).contains(item1, item2);
     }
     @Test
     void updateItem() {
-        //given
+//given
         Item item = new Item("item1", 10000, 10);
-
         Item savedItem = itemRepository.save(item);
         Long itemId = savedItem.getId();
-        //when
+//when
         Item updateParam = new Item("item2", 20000, 30);
         itemRepository.update(itemId, updateParam);
-        //then
-        Item findItem = itemRepository.findBy(itemId);
-
+        Item findItem = itemRepository.findById(itemId);
+//then
         assertThat(findItem.getItemName()).isEqualTo(updateParam.getItemName());
         assertThat(findItem.getPrice()).isEqualTo(updateParam.getPrice());
         assertThat(findItem.getQuantity()).isEqualTo(updateParam.getQuantity());
